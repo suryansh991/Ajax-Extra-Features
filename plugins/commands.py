4d90b7b8ef853e90d102f1c72a3d45d3ebaffd58
Zinan100 <97530806+Zinan100@users.noreply.github.com>
2022-02-04 12:00:03 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -27,8 +27,11 @@ async def start(client, message):
             ],
             [
                 InlineKeyboardButton('♻️ HΞLᎮ ♻️', url=f"https://t.me/{temp.U_NAME}?start=help")
+            ],
+            [
+                InlineKeyboardButton('owner', url=f"t.me/zinan00100)
             ]
-            ]
+            
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
